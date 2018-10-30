31915045971d5b50307b738223e2624d1790251b
Joshua Franklin <joshuafranklin26@gmail.com>
2018-10-30 12:51:56 -0600

    Made appropriate variables constant.

--- demo-boilerplate/index.js
+++ demo-boilerplate/index.js
@@ -1,6 +1,6 @@
-let startButton = document.getElementById('start-button');
-let canvas = document.getElementById('canvas');
-let context = canvas.getContext('2d');
+const startButton = document.getElementById('start-button');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 
 startButton.addEventListener('click', function () {
     startButton.style.display = 'none';
